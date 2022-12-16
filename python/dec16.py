69c34d8a0fc47e5449fe94302ad51c8a34b8331d
alakhpandya <alakhpandya@gmail.com>
2022-12-16 17:01:25 +0530

    Create dec16.py

---
+++ python/dec16.py
@@ -0,0 +1,51 @@
+# a = int(input("Enter three integers:\n"))
+# b = int(input())
+# c = int(input())
+"""
+if a > b:
+    if b > c:
+        print("a > b > c")
+    else:
+        print("a > b and c >= b")
+    print("a > b")
+print("a is not greater than b")
+"""
+"""
+if a > b and a > c:
+    print("a is greatest")
+"""
+"""
+if a > b > c:
+    print("a > b > c")
+"""
+
+# loop: while, for
+
+a = int(input("Enter any integer: "))
+i = 1
+while i <= 10:
+    print(f"{a} x {i} = {a * i}")
+    i += 1
+
+"""
+1. Write a Python program that asks two integers from user & print all the numbers between them, each in new line.
+2. Write a Python program that asks two integers from user & print all the even numbers between them, each in new line.
+3. Write a Python program that asks two integers from user & print all the odd numbers between them in reverse order, each in new line.
+4. Write a Python program to print all the members of the following list each in new line:
+fruits = ["banana", "guava", "strawberries", "kiwi", "mango", "apple", "orange"]
+"""
+
+
+# loop examples:
+"""
+1. Write a Python code that takes an integer from user and prints number of digits in that integer.
+2. Write down a Python code that creates a user defined list
+3. Write a Python code to print each of the elements of a given list in a new line
+4. Write a Python program that prints whether the number given is a prime number or not.
+5. Write a Python program that prints whether the number given is a perfect number or not.
+6. Write a Python program that prints whether the number given is an Armstrong number or not.
+7. Write a Python program that prints all the prime numbers between two integers given by user.
+8. Write a Python program that prints all the perfect numbers between two integers given by user.
+9. Write a Python program that prints all the Armstrong numbers between two integers given by user.
+
+"""
