18f84311dd9e2c00c9d517d4e88dace7fe77ee81
alakhpandya <alakhpandya@gmail.com>
2022-12-28 17:06:33 +0530

    Create dec28.py

---
+++ python/dec28.py
@@ -0,0 +1,56 @@
+# for loop in range:
+
+# range(5) = 0,1,2,3,4
+# i=0; i<5; i++:
+# for i in range(5):
+#     print(i)
+
+# i=5, i<15; i++
+# for i in range(5, 15):
+#     print(i)
+
+# i=5; i<30; i+=2
+# for i in range(5, 30, 2):
+#     print(i)
+# for odd_nunber in range(5, 30, 2):
+#     print(odd_nunber)
+
+# i=10; i>0; i--
+# for i in range(10, 0, -1):
+#     print(i)
+
+# fruits = ["apple", "mango", "kiwi", "banana", "strawberry", "orange"]
+# for fruit in fruits:
+# for fruits in fruits:
+#     if fruit == 'banana':
+#         break
+#     print(fruit)
+
+
+# name = "Dharmangi"
+# for alphabet/letter/character in name:
+# for letter in name:
+#     print(letter)
+
+
+# fruits = ["apple", "mango", "kiwi", "banana", "strawberry", "orange"]
+# for i in range(len(fruits)-1, -1, -1):
+# length = len(fruits)
+# for i in range(length):    # i = 0,1,2,3,4,5
+    # print(fruits[length-i-1])
+
+# creating user defined list
+"""
+myList = []
+n = int(input("Enter no of elements: "))
+print(f"Enter {n} elements:")
+for x in range(n):
+    # element = input()
+    # myList.append(element)
+    myList.append(input())
+print(myList)
+"""
+
+# Write a program that generates a list containing integers from 1 to 100 (including both) and print it.
+
+# Next Class: list comprehension
