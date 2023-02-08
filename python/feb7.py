f7b8b3f9fdb24761276b3fae34933f07c09fda99
alakhpandya <alakhpandya@gmail.com>
2023-02-08 18:04:38 +0530

    Create feb7.py

---
+++ python/feb7.py
@@ -0,0 +1,17 @@
+"""
+r = {
+    'alakh' : 34,
+    'tithi' : 19,
+    'khush' : 35,
+    'krushnam' : 28,
+    'dharmangi' : 38
+}
+
+# for x in r:     # x = keys of r
+
+# for x in r.items():                   # x = (key, value) in r
+# for student, marks in r.items():      # student = keys & marks = values of r
+
+"""
+
+# Write a Python function that prints whether the number given by user is prime or not. Also write a main program that asks an integer from user & passes to the function.
