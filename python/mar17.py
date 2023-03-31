c063c35bd594aa4a7ecc42221cda149ecb721907
alakhpandya <alakhpandya@gmail.com>
2023-03-31 20:03:03 +0530

    mar31

--- python/mar17.py
+++ python/mar17.py
@@ -25,7 +25,7 @@ Functions Examples:
 
 Design a Banking App in Python that has the following functionalities:-
 User can:-
-◆OPEN ACCOUNT by username and password of his choice. On Opening account, his initial balance will be ₹ 25,000/-. Once he opens account, he can login by re-entering the same username & password.
+◆OPEN ACCOUNT by username and password of her/his choice. On Opening account, his initial balance will be ₹ 25,000/-. Once he opens account, he can login by re-entering the same username & password.
 ◆LOGIN is compulsory to perform any task such as withdrawal, deposit or balance check. If the user name or password do not match, he can not Login. Once he is logged in, he can do as many transactions as he wants. He needs to Logout after he finishes all the transactions
 ◆DEPOSIT will enable user to deposit amount of money of his choice. His balance should be updated after the task completes.
 ◆WITHDRAW will enable user to withdraw amount of money of his choice. The only condition is that his balance at any point can not go less than ₹10,000/-. If this can happen after his withdrawal, your program must not allow that transaction. His balance should be updated after the task completes.
---
+++ python/mar31.py
@@ -0,0 +1,51 @@
+# def armstrong(a):   # 153
+#     return True if sum([int(digit)**len(str(a)) for digit in str(a)]) == a else False
+
+"""
+armstrong = lambda a : True if sum([int(digit)**len(str(a)) for digit in str(a)]) == a else False
+    
+x = int(input("Lower limit: "))
+y = int(input("Upper limit: "))
+print(f"Armstrong numbers between {x} & {y} are:")
+for i in range(x, y+1):
+    if armstrong(i): 
+        print(i)
+"""
+# some imp built-in functions: map, filter, reduce
+"""
+def sqrt(n):
+    return n ** 0.5
+myList = [16,25,36,49,81]
+
+# sqrtList = []
+# for num in myList:
+#     sqrtList.append(sqrt(num))
+sqrtList = list(map(sqrt, myList))
+
+print(sqrtList)
+# o/p: [4.0, 5.0, 6.0, 7.0, 9.0]
+"""
+"""
+def fact(n):
+
+yourList = [2,3,4,5,6,7,8]
+
+# Write your code using that factorial function & map here
+
+print(factList)
+# o/p: [2, 6, 24, 120, 720, 5040, 40320]
+"""
+
+
+# filter function
+
+# Write a function here, that returns True or False based on whether the number given in its argument is Prime or not.
+
+num_list = [x for x in range(55, 155)]
+
+# Write additional code here that uses that function
+
+print(primeList)
+# output list must have only prime numbers from 55 to 154
+
+
