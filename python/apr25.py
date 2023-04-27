012dc4925f5bbbdc5eb0b52fcad6ddd5d05431ed
alakhpandya <alakhpandya@gmail.com>
2023-04-27 05:34:03 +0530

    Update apr25.py

--- python/apr25.py
+++ python/apr25.py
@@ -120,7 +120,124 @@
     B)	No Output
     C)	Below Freezing
     D)	Error
-
-
+"""
+from sys import getsizeof
+l1 = [x for x in range(1, 101)]
+squares = map(lambda n : n ** 2, l1)
+# print(getsizeof(squares))
+for x in squares:
+    print(x, end=", ")
 
 """
+1.	Class Performance 2
+    Problem Description
+    You have been given a dataset for marks of 2 subjects, scored by students of classes ClassA and ClassB. You now want to compare the performances of class A and class B by finding out the average performance of the classes. Write a program to find if class A performed better. Print True is Class A is strictly better else return False.
+    Input Format
+    There are 4 lines in the input.
+    The first and second lines are marks of subjects for Class A.
+    The third and fourth lines are marks of subjects for Class B.
+    Output Format
+    Print True if class A is strictly better else False.
+    Example Input
+    Input 1:-
+    80
+    27
+    54
+    61
+    Input 2:-
+    54
+    61
+    80
+    27
+    Example Output
+    Output 1:-
+    False
+    Output 2:-
+    True
+    Sample Exaplanation
+    Explanation 1:-
+    The average marks of class A is (80 + 27)/2 = 53.5, and the average of class B is (54 + 61)/2 = 57.5, so class B's average
+    is better hence False is printed.
+
+    Explanation 2:-
+    The average marks of class A is(54 + 61)/2 = 57.5, and the average of class B is(80 + 27)/2 = 53.5, so class B's average
+    is better hence True is printed.
+
+2.	Min of two
+    Problem Description
+    Write a program to input two numbers(A & B) from user and print the minimum element among A & B in each line.
+    Problem Constraints
+    1 <= A <= 1000000
+    1 <= B <= 1000000
+
+    Input Format
+    First line is a single integer A.
+    Second line is a single integer B.
+
+    Output Format
+    One line containing an integer as per the question.
+
+    Example Input
+    Input 1:
+    5 
+    6
+    Input 2:
+    1000 
+    10000
+    Example Output
+    Output 1:
+    5
+    Output 2:
+    1000
+
+3.	All correct about x
+    You have been given the following piece of code. Assume that x has already been declared.
+
+    if x > 2:
+        x = x*2
+    if x > 4:
+        x = 0
+    print(x)
+    Select the correct statement regarding this code.
+    A.	Output will always be equal to 0
+    B.	If x > 2 and x < 4, final value of x will be double the initial value
+    C.	For x in 0 < x < 4 the output will always be 0
+    D.	For x > 2, output will be equal to 0
+
+4.	If-else output
+    What will be the output of the following code:
+    x = 2
+    if x==2:
+        x = 3
+    x = 4
+    else:
+        x = 5
+    print(x)
+
+    A.	3
+    B.	4
+    C.	5
+    D.	Error
+
+ 
+5.	Guide the coordinate
+    Problem Description
+    Given the (x, y) coordinates of a point on a 2D plane, write a program that transforms these coordinates according to the conditions given below.
+    •	If the sum of x and y coordinates is a multiple of 5, then increment both the coordinates by 1 and print.
+    •	If the x coordinate is even and the y coordinate is odd, then increment y by 1 and print both.
+    •	If the y coordinate is even and the x coordinate is odd, then increment x by 1 and print both.
+    Input Format
+    Two lines containing integer x and y.
+    Output Format
+    Two lines containing transformed integer values of x and y.
+    Sample Input
+    15
+    10
+    Sample Output
+    16
+    11
+"""
+
+
+# Next Class: time, timeit, datetime modules
+
