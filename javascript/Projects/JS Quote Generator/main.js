4947ec5ea9484c355158b39feb0669381c03145f
alakhpandya <alakhpandya@gmail.com>
2023-05-05 14:03:40 +0530

    may4

---
+++ javascript/Projects/JS Quote Generator/main.js
@@ -0,0 +1,57 @@
+// getting a random number:
+// console.log(Math.random());
+
+// getting a random number >= 5
+// console.log(Math.random() + 5);
+
+// getting a random number between 0 to 10
+// console.log(Math.random() * 10)
+
+// getting an integer between 0 to 10
+// console.log(Math.floor(Math.random() * 10))
+
+// getting an integer between 7 to 20
+// console.log(Math.floor(Math.random() * 13) + 7)
+
+allQuotes = [
+    {
+        quote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        author: "Martin Fowler"
+    },
+    {
+        quote: "",
+        author: ""
+    },
+    {
+        quote: "",
+        author: ""
+    },
+    {
+        quote: "",
+        author: ""
+    },
+    {
+        quote: "",
+        author: ""
+    },
+    {
+        quote: "",
+        author: ""
+    },
+    {
+        quote: "",
+        author: ""
+    },
+    {
+        quote: "",
+        author: ""
+    },
+    {
+        quote: "",
+        author: ""
+    },
+    {
+        quote: "",
+        author: ""
+    },
+]
